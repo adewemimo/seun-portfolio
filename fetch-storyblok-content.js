6fc2af8d52170e483407c6506899f9354718c2c7
Seun Soetan <seun@strata.ltd>
2024-03-21 19:24:10 -0400

    feat: storyblok setup for use in axe

---
+++ fetch-storyblok-content.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+async function fetchStoryblokContent(version, token, lang) {
+ const url = `https://api.storyblok.com/v2/cdn/stories/landing-page?version=${version}&token=${token}&cv=1701903822&language=${lang}`;
+ const response = await fetch(url, {cache: "no-store"});
+ const data = await response.json();
+ // Write data to a temporary file
+ fs.writeFileSync('./storyblok-data.json', JSON.stringify(data));
+}
+
+// Replace these with your actual Storyblok version, token, and language
+const version = draft;
+const token = 4RWgsQjWWNFCzfqgaSO2PAtt;
+const lang = en;
+
+fetchStoryblokContent(version, token, lang).catch(console.error);
